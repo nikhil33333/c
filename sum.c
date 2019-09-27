e1285601e1a8f11da2b037b4dc7a9c3bf9a01865
nikhil33333 <55886611+nikhil33333@users.noreply.github.com>
2019-09-27 20:58:37 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,28 @@
+int main()
+
+{
+    
+float a,b,c,sum,avg;
+    
+printf("enter the a");
+    
+scanf("%f",&a);
+    
+printf("enter the b");
+    
+scanf("%f",&b);
+    
+printf("enter the c");
+    
+scanf("%f",&c);
+    
+sum=(a+b+c);
+    
+avg=sum/3;
+    
+printf("the sum is %.2f",sum);
+    
+printf("the average is %.2f",avg);
+    
+
+}
