6ca8f0b4a81686d39323ccf237dc1bee029de905
nikhil33333 <55886611+nikhil33333@users.noreply.github.com>
2019-09-27 20:46:28 +0530

    Add files via upload

---
+++ degree conversion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+#include<math.h>
+
+int degree()
+
+{
+    
+float a,b;
+    
+printf("enter the temperature in celsius");
+    
+scanf("%f",&a);
+    
+b=((1.8*a)+32);
+    
+printf("the temperature in farenheit is %f",b);
+
+}
