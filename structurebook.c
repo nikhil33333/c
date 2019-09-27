0ce53c8f8e57d9fb92f335731615a774d9c09570
nikhil33333 <55886611+nikhil33333@users.noreply.github.com>
2019-09-27 22:29:22 +0530

    Add files via upload

---
+++ structurebook.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+void bookprint(struct Book);
+ 
+struct Book
+
+{
+ 
+char name[100];
+ 
+char author[100];
+ 
+char genre[10];
+
+}
+
+b;
+
+main()
+
+{
+ 
+ 
+printf("Enter book name\n");
+ 
+scanf("%s",&b.name);
+ 
+printf("Enter book author\n");
+ 
+scanf("%s",&b.author);
+  
+printf("Enter book name\n");
+ 
+scanf("%s",&b.genre);
+ bookprint(b);
+
+
+}
+
+void bookprint(struct Book b1)
+
+{
+ printf("Book Name=%s\n",b1.name);
+
+ printf("Book author=%s\n",b1.author);
+ 
+printf("Book genre=%s\n",b1.genre);
+
+
+}
