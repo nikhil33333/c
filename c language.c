fef9c57daf2947623aa4940cb208e6a1abe495e5
nikhil33333 <55886611+nikhil33333@users.noreply.github.com>
2019-09-27 20:43:22 +0530

    Add files via upload

---
+++ c language.c
@@ -0,0 +1,28 @@
+int main()
+
+{
+
+    
+char name[100];
+  
+int age,phoneNum;
+ 
+printf("enter the name \t");
+    
+scanf("%s",name);
+  
+printf("enter the age \t");
+ 
+scanf("%d",&age);
+  
+printf("enter the phonenumber \t");
+   
+scanf("%d",&phoneNum);
+  
+printf("the name is %s \n",name);
+  
+printf("the age is %d \n",age);
+   
+printf("the phonenumber is %d \n",phoneNum);
+
+}
